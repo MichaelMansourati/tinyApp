8c5f3fbe7af0c52aea73c5b296feec2848a949fe
Michael Mansourati <mmansourati@gmail.com>
2017-03-20 02:16:12 +0000

    okay NOW it's really done

--- express_server.js
+++ express_server.js
@@ -1,3 +1,6 @@
+//------...THINGS...------//
+
+
 
 const express = require("express");
 const app = express();
@@ -12,6 +15,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
+
+
+//------DATABASES------//
+
+
+
 const urlDatabase = {
   "aaa111": {
     b2xVn2: "http://www.lighthouselabs.ca"
@@ -20,6 +29,8 @@ const urlDatabase = {
     "9sm5xK": "http://www.google.com"
   }
 }
+
+
 
 const urlDatabasePublic = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -31,12 +42,16 @@ const urlDatabasePublic = {
 const users = {
   "dummyID": {
     id: "dummyID",
-    email: "dummyEmail@fake.com",
+    email: "dummyEmail@fake.com", //this dummyID exists for a reason. just FYI.
     password: "dummyhead"
   }
 }
 
+
+
 //------------LISTEN------------//
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -44,8 +59,9 @@ app.listen(PORT, () => {
 
 
 
-
 //-------------GET-------------//
+
+
 
 app.get("/", (req, res) => {
   const currentUser =  users[req.cookies.id];
@@ -55,6 +71,7 @@ app.get("/", (req, res) => {
     res.redirect("/login");
   }
 });
+
 
 
 app.get("/urls", (req, res) => {
@@ -69,8 +86,8 @@ app.get("/urls", (req, res) => {
   } else {
     res.status(401).send("<p>You must be logged in to access this function. <a href='/login'>login here</a></p>");
   }
-  console.log(urlDatabasePublic);
 });
+
 
 
 app.get("/urls/new", (req, res) => {
@@ -86,13 +103,6 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
-/*
-to make user-specific databases, all I have to is go from the url creation point and go forward,
-simply pushing each new url into the database of the user who created it. the example urls that
-exist already are for test purposes! They can either be deleted or hard-coded into those user-specific
-databases. databases should be dynamically created upon the first url creation in each new account.
-
-*/
 
 
 app.get("/urls/:id", (req, res) => {
@@ -100,22 +110,18 @@ app.get("/urls/:id", (req, res) => {
   if (!currentUser) {
     res.status(401).send("<p>You must be logged in to access this function. <a href='/login'>login here</a></p>");
   }
-
   const templateVars = {
     'id': [req.params.id],
     shortURL: `localhost:8080/u/${[req.params.id]}`,
     user: currentUser
   };
-  console.log("req.params.id: ", req.params.id);
-  console.log("")
   if (urlDatabase[currentUser.id][req.params.id]) {
-    templateVars.longURL = `localhost:8080/u/${req.params.id}`;
+    templateVars.longURL = urlDatabase[currentUser.id][req.params.id];
     res.render("urls_show", templateVars);
     res.status(200);
   } else if (urlDatabasePublic[req.params.id]) {
-    // console.log(urlDatabasePublic[theId]);
     res.status(403).send("This short url belongs to another user and may only be modified them.");
-  } else if (!urlDatabase[currentUser.id][req.params.id]) {
+  } else {
     res.status(404).send("This short url does not exist");
   }
 });
@@ -138,17 +144,10 @@ app.get("/login", (req, res) => {
 });
 
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-
 
 app.get(`/u/:id`, (req, res) => {
-  console.log('req.params: ', req.params);
   const longURL = urlDatabasePublic[req.params.id];
   const shortURL = (`http://localhost:8080/u/${req.params.id}`);
-  console.log('longURL: ', longURL);
 
   if(longURL) {
     res.redirect(longURL);
@@ -156,6 +155,7 @@ app.get(`/u/:id`, (req, res) => {
     res.status(404).send('Sorry, we cannot find that!');
   }
 });
+
 
 
 app.get("/register", (req, res) => {
@@ -174,15 +174,14 @@ app.get("/register", (req, res) => {
 
 
 
-
 //-------------POST-------------//
+
+
 
 app.post("/urls/:id", (req, res) => {
   const currentUser = users[req.cookies.id];
-  console.log(req.params.id);
   urlDatabase[currentUser.id][req.params.id] = req.body.longURL;
   urlDatabasePublic[req.params.id] = req.body.longURL;
-  console.log(urlDatabasePublic);
   res.redirect("/urls");
 })
 
@@ -191,7 +190,6 @@ app.post("/urls/:id", (req, res) => {
 app.post("/login", (req, res) => {
   let userEmail = req.body.email;
   let userPassword = req.body.password;
-  console.log('req.body: ', req.body);
   let foundUser = checkLogin(users, userEmail, userPassword);
 
   if (userEmail && userPassword) {
@@ -223,16 +221,14 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 
+
 app.post("/register", (req, res) => {
   let id = generateRandomString();
   let email = req.body.email;
   let hashPassword = bcrypt.hashSync(req.body.password, 10);
-  console.log("req.body: ", req.body);
 
   if (email && hashPassword) {
-    console.log(`email: ${email} and hashPassword: ${hashPassword}`);
     if (validateEmail(users, email) === true) {
-      console.log("emailval successful");
       users[id] = {
           'id': id,
           'email': email,
@@ -242,7 +238,6 @@ app.post("/register", (req, res) => {
       res.cookie("id", id);
       res.redirect("/");
     } else {
-      console.log("emailval failed??");
       res.status(400).send("The email address you have chosen is already in use. Please choose another.");
     };
   } else {
@@ -255,9 +250,6 @@ app.post("/register", (req, res) => {
 app.post("/urls", (req, res) => {
   const currentUser = users[req.cookies.id];
   let urlId = generateRandomString();
-  console.log("currentUser.id: ", currentUser.id);
-  console.log("req.body.longURL: ", req.body.longURL)
-  // console.log("urlDatabase[currentUser[id]]: ", urlDatabase[currentUser[id]])
   if (currentUser) {
     if (!urlDatabase[currentUser.id]) {
       urlDatabase[currentUser.id] = {};
@@ -265,7 +257,6 @@ app.post("/urls", (req, res) => {
     urlDatabase[currentUser.id][urlId] = req.body.longURL;
     urlDatabasePublic[urlId] = req.body.longURL;
     res.redirect(`/urls/${urlId}`);
-    console.log(urlDatabase);
   } else {
     res.status(401).send("<p>You must be logged in to access this function. <a href='/login'>login here</a></p>");
   }
@@ -275,6 +266,9 @@ app.post("/urls", (req, res) => {
 
 
 //----------/   FUNCTIONS   \----------//
+
+
+
 
 function generateRandomString() {
   let text = "";
@@ -304,7 +298,6 @@ function checkLogin (database, userEmail, userPassword) {
   let usersArr = Object.keys(database).map((id) => {
     return database[id];
   })
-  console.log("usersArr: ", usersArr);
   let foundUser = usersArr.find((user) => {
     return user.email === userEmail && bcrypt.compareSync(userPassword, user.hashPassword);
   })
